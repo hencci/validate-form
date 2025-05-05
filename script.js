c6d24c6694eb733257991d7fc48fb499aed9eab5
hencci <hencci20@gmail.com>
2025-05-05 12:04:35 +0100

    Create functions needed

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ const postal = document.getElementById('postal');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmPassword');
 const highFive = document.getElementById('highFive');
+
+function validateEmail(input) {
+
+}
+
+function validateCountry(input) {
+
+}
+
+function validatePostalCode(input) {
+
+}
+
+function validatePassword(input) {
+    
+}
+function validateConfirmPassword(input) {
+
+}
+
+function showError(input, message) {
+
+}
+
+function clearError(input) {
+
+}
+
+function validateField(event) {
+    
+}
