bfca84b30a0f52e6d5b07a83784ee37ac087601e
hencci <hencci20@gmail.com>
2025-05-05 11:38:18 +0100

    Select DOM, save in variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('signupForm');
+const email = document.getElementById('email');
+const country = document.getElementById('country');
+const postal = document.getElementById('postal');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+const highFive = document.getElementById('highFive');
